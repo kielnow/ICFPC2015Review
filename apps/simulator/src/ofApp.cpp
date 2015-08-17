45907ce5cd54aa3d4e827a4817f73f2af1399c67
kielnow <etsu1here@gmail.com>
2015-08-18 04:10:19 +0900

    GUIのテスト、日本語フォント描画のテスト、addonsプロジェクトを作成、プロジェクト設定を整理

--- apps/simulator/src/ofApp.cpp
+++ apps/simulator/src/ofApp.cpp
@@ -2,7 +2,18 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	ofSetWindowTitle("simulator");
+	ofSetVerticalSync(true);
+	ofSetFrameRate(60);
 
+	ofxTrueTypeFontUC::setGlobalDpi(72);
+	font.loadFont("Osaka.ttc", 100, true, true);
+	font.setLineHeight(font.getFontSize() * 1.5f);
+
+	gui.setup(); // most of the time you don't need a name
+	gui.add(screenSize.setup("screen size", ofToString(ofGetWidth()) + "x" + ofToString(ofGetHeight())));
+
+	bHide = false;
 }
 
 //--------------------------------------------------------------
@@ -12,12 +23,21 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+	ofBackgroundGradient(ofColor::white, ofColor::gray);
 
+	ofSetColor(0);
+	font.drawString("Hello, World.\nÇ±ÇÒÇ…ÇøÇÕÅAê¢äEÅB", 100, 200);
+
+	if (!bHide) {
+		gui.draw();
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+	if (key == 'h') {
+		bHide = !bHide;
+	}
 }
 
 //--------------------------------------------------------------
@@ -57,7 +77,7 @@ void ofApp::mouseExited(int x, int y){
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+	screenSize = ofToString(w) + "x" + ofToString(h);
 }
 
 //--------------------------------------------------------------
